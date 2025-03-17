e6521255b9222245935e2a463c04eabc81a0ad91
Emilio <emilio.jimenezlz@udlap.mx>
2025-03-17 15:21:24 -0600

    new files added

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import gymnasium as gym
 import numpy as np
 
-env = gym.make('FrozenLake-v1', render_mode='human')
-observation, info = env.reset()
-
-episode_over = False
-while not episode_over:
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
-
-    episode_over = terminated or truncated
-
-env.close()
+env = gym.make('Blackjack-v1', sab=True, render_mode = "rgb_array")
+obs,info = env.reset()
